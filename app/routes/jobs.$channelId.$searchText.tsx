7af5a5d58665d10297378c79d367055a7f76e6c2
Cam Sima <cjsima@gmail.com>
2023-07-05 11:22:21 -0400

    incrementer

--- app/routes/jobs.$channelId.$searchText.tsx
+++ app/routes/jobs.$channelId.$searchText.tsx
@@ -1,6 +1,6 @@
 import { useFetcher, useLoaderData, useNavigate } from "@remix-run/react";
 import { LoaderArgs } from "@remix-run/server-runtime";
-import { useEffect } from "react";
+import { useEffect, useReducer, useState } from "react";
 
 import { json } from "react-router";
 import Spinner from "~/components/Spinner";
@@ -14,7 +14,13 @@ export default function JobStatusPage() {
   const navigate = useNavigate();
   const fetcher = useFetcher<typeof loader>();
   const { channelId, searchText } = useLoaderData<typeof loader>();
+  const [stats, setStats] = useState({
+    numVideosFound: 0,
+    numVideosProcessed: 0,
+    numVideosFailed: 0,
+  });
 
+  // effect for loading new data
   useEffect(() => {
     const interval = setInterval(() => {
       const jobStatus = fetcher.data?.job?.status;
@@ -26,6 +32,38 @@ export default function JobStatusPage() {
     }, 1000);
     return () => clearInterval(interval);
   }, [fetcher.data?.job?.status]);
+
+  // effect for incrementing the video stats by one from old value to new value
+  useEffect(() => {
+    const currentStats = {
+      numVideosFound: fetcher.data?.job?.numVideosFound || 0,
+      numVideosProcessed: fetcher.data?.job?.numVideosProcessed || 0,
+      numVideosFailed: fetcher.data?.job?.numVideosFailed || 0,
+    };
+
+    const incrementInterval = setInterval(() => {
+      setStats((prevStats) => ({
+        numVideosFound:
+          prevStats.numVideosFound < currentStats.numVideosFound
+            ? prevStats.numVideosFound + 1
+            : currentStats.numVideosFound,
+        numVideosProcessed:
+          prevStats.numVideosProcessed < currentStats.numVideosProcessed
+            ? prevStats.numVideosProcessed + 1
+            : currentStats.numVideosProcessed,
+        numVideosFailed:
+          prevStats.numVideosFailed < currentStats.numVideosFailed
+            ? prevStats.numVideosFailed + 1
+            : currentStats.numVideosFailed,
+      }));
+    }, 10);
+
+    return () => clearInterval(incrementInterval);
+  }, [
+    fetcher.data?.job?.numVideosFound,
+    fetcher.data?.job?.numVideosProcessed,
+    fetcher.data?.job?.numVideosFailed,
+  ]);
 
   return (
     <div className="flex h-screen flex-col items-center justify-center">
@@ -40,13 +78,13 @@ export default function JobStatusPage() {
             {fetcher.data?.job?.status || "pending"}
           </div>
           <div className="text-2xl font-bold">
-            {formatNumber(fetcher.data?.job?.numVideosFound || 0)}
+            {formatNumber(stats.numVideosFound || 0)}
           </div>
           <div className="text-2xl font-bold">
-            {formatNumber(fetcher.data?.job?.numVideosProcessed || 0)}
+            {formatNumber(stats.numVideosProcessed || 0)}
           </div>
           <div className="text-2xl font-bold">
-            {formatNumber(fetcher.data?.job?.numVideosFailed || 0)}
+            {formatNumber(stats.numVideosFailed || 0)}
           </div>
         </div>
         <div className="justif-start flex flex-col">
